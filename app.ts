69a3a2e337df960b3f91718ea031d3a9bbed5bbe
redreev8 <redreevs@mail.ru>
2024-12-03 19:34:44 +0500

    fix babel

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import fileRouter from './controller/files/router'
 import folderRouter from './controller/folders/router'
+import createFolderStore from './helper/create-folder-store'
 
 dotenv.config()
 
@@ -13,6 +14,8 @@ app.use(express.json())
 app.use(cors())
 app.use('/api', fileRouter)
 app.use('/api', folderRouter)
+
+createFolderStore()
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
